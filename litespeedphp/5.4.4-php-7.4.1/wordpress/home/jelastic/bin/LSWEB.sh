f2b89eb935e815bb9d0403b879b34d3cbf0a9318
GS <gs@jelastic.com>
2020-02-24 15:37:19 +0000

    Added scripts for custom configuration

---
+++ litespeedphp/5.4.4-php-7.4.1/wordpress/home/jelastic/bin/LSWEB.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DEFAULT_LSWS_CONFIG="/var/www/conf/httpd_config.xml"
+DEFAULT_VIRTUALHOST_CONFIG="/var/www/webroot/vhconf.xml"
+PHPMYADMIN_VIRTUALHOST_CONFIG="/usr/share/phpMyAdmin/vhost.conf"
+ED_CMD="ed --inplace"
+
+cp -f "${DEFAULT_LSWS_CONFIG}" ${DEFAULT_LSWS_CONFIG}.backup.$(date +%d-%m-%Y.%H:%M:%S.%N) || exit 1
+cp -f "${DEFAULT_VIRTUALHOST_CONFIG}" ${DEFAULT_VIRTUALHOST_CONFIG}.backup.$(date +%d-%m-%Y.%H:%M:%S.%N) || exit 1
+[ -f "${PHPMYADMIN_VIRTUALHOST_CONFIG}" ] && { cp -f "${PHPMYADMIN_VIRTUALHOST_CONFIG}" ${PHPMYADMIN_VIRTUALHOST_CONFIG}.backup.$(date +%d-%m-%Y.%H:%M:%S.%N) || exit 1; }
+
+if [ -e ${DEFAULT_LSWS_CONFIG} ]; then 
+###JE-51695-Disable compression on cp layer for WP cluster
+  /usr/bin/xmlstarlet $ED_CMD -u "httpServerConfig/tuning/enableGzipCompress" -v "0" ${DEFAULT_LSWS_CONFIG} 2>&1;
+fi
+
+sudo service lsws reload
