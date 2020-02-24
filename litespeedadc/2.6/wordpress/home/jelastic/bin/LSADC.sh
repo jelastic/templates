0d24dd905471810296ec83c7711d07cb26929ec1
sych74 <sych74@gmail.com>
2020-02-24 19:43:23 +0200

    Update LSADC.sh

--- litespeedadc/2.6/wordpress/home/jelastic/bin/LSADC.sh
+++ litespeedadc/2.6/wordpress/home/jelastic/bin/LSADC.sh
@@ -3,6 +3,3 @@
 LSLB_CONF="/var/www/conf/lslbd_config.xml"
 VH_CONF="/var/www/conf/jelastic.xml"
 ED_CMD="ed --inplace"
-
-cp -f ${LSLB_CONF} ${LSLB_CONF}.backup.$(date +%d-%m-%Y.%H:%M:%S.%N) || exit 1
-
