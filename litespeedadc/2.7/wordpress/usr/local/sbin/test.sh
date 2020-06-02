46f5e7bb70d709f5e4cd9a19cff5c06fb4056e4d
GS <gs@jelastic.com>
2020-06-02 15:32:22 +0000

    Scripts for opt test

--- litespeedadc/2.7/wordpress/usr/local/sbin/test.sh
+++ litespeedadc/2.7/wordpress/usr/local/sbin/test.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-OPTIMIZATION_LIST="optimization.list"
-OPTIMIZATION_OUTPUT="/tmp/optimization.output"
+OPTIMIZATION_LIST="/usr/local/sbin/optimization.list"
+TEST_OUTPUT="/tmp/test.output"
 LSLB_CONF="/var/www/conf/lslbd_config.xml"
 VH_CONF="/var/www/conf/jelastic.xml"
 SEL_CMD="sel -t -v"
 
-[ -f $OPTIMIZATION_OUTPUT ] && rm -f $OPTIMIZATION_OUTPUT
+echo "" > $TEST_OUTPUT
 while read -ru 4 PARAM; do
-  [[ $PARAM = \#* ]] && continue
-  [[ $PARAM = loadBalancerConfig* ]] && value=($(/usr/bin/xmlstarlet ${SEL_CMD} ${PARAM%=*} $LSLB_CONF))
-  [[ $PARAM = virtualHostConfig* ]] && value=($(/usr/bin/xmlstarlet ${SEL_CMD} ${PARAM%=*} $VH_CONF))
-
-  echo "${PARAM%=*}=${value[0]}" >> $OPTIMIZATION_OUTPUT
+  [[ $PARAM = loadBalancerConfig* ]] && value=($(/usr/bin/xmlstarlet ${SEL_CMD} ${PARAM%=*} $LSLB_CONF));
+  [[ $PARAM = virtualHostConfig* ]] && value=($(/usr/bin/xmlstarlet ${SEL_CMD} ${PARAM%=*} $VH_CONF));
+  [[ ${PARAM#*=} != ${value} ]] && echo "PARAMETER: ${PARAM%=*} EXPECTED: ${PARAM#*=} ACTUAL: ${value[0]}" >> $TEST_OUTPUT
 done 4< $OPTIMIZATION_LIST
 
