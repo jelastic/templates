4be17110b3f8a4507547e45efe3275ccc880c255
GS <gs@jelastic.com>
2021-05-19 07:55:23 +0000

    Added config for magento

--- litespeedphp/default/magento/usr/local/sbin/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OPTIMIZATION_LIST="/usr/local/sbin/optimization.list"
-TEST_OUTPUT="/tmp/test.output"
-WEBROOT_ROOT="/var/www/webroot/ROOT"
-WP=`which wp`
-APP_VERSION=$(${WP} core version --path=${WEBROOT_ROOT})
-echo "APP_VERSION: ${APP_VERSION}" > $TEST_OUTPUT
-
