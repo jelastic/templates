0c692194c1b0916c6fc1bca7fed21d440eac329a
sych74 <sych74@gmail.com>
2021-06-18 15:33:38 +0300

    Change Doc root

--- llsmp/default/magento/usr/local/sbin/optimization.sh
+++ llsmp/default/magento/usr/local/sbin/optimization.sh
@@ -7,6 +7,7 @@ ED_CMD="ed --inplace"
 [ -f "/var/www/conf/vhconf.xml" ] && DEFAULT_VIRTUALHOST_CONFIG="/var/www/conf/vhconf.xml";
 
 if [ -e ${DEFAULT_VIRTUALHOST_CONFIG} ]; then
+  /usr/bin/xmlstarlet ${ED_CMD} -u "virtualHostConfig/docRoot" -v "\$VH_ROOT/ROOT/pub/" "${DEFAULT_VIRTUALHOST_CONFIG}" 2>/dev/null;
   /usr/bin/xmlstarlet ${ED_CMD} -s "virtualHostConfig/cache/storage" -t elem -n "litemage" -v "1" "${DEFAULT_VIRTUALHOST_CONFIG}" 2>/dev/null;
 fi
 
