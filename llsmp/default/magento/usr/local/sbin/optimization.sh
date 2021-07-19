d0b2c42cc7af7ba1e9d375ff4dd3062a9d3a5202
sych74 <sych74@gmail.com>
2021-07-19 11:29:31 +0300

    Fixed litemage

--- llsmp/default/magento/usr/local/sbin/optimization.sh
+++ llsmp/default/magento/usr/local/sbin/optimization.sh
@@ -8,7 +8,6 @@ ED_CMD="ed --inplace"
 
 if [ -e ${DEFAULT_VIRTUALHOST_CONFIG} ]; then
   /usr/bin/xmlstarlet ${ED_CMD} -u "virtualHostConfig/docRoot" -v "\$VH_ROOT/ROOT/pub/" "${DEFAULT_VIRTUALHOST_CONFIG}" 2>/dev/null;
-  /usr/bin/xmlstarlet ${ED_CMD} -s "virtualHostConfig/cache/storage" -t elem -n "litemage" -v "1" "${DEFAULT_VIRTUALHOST_CONFIG}" 2>/dev/null;
 fi
 
-sudo service lsws reload
+sudo jem service restart
