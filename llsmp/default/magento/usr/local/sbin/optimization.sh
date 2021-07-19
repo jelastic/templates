07c3b4ea13f750c162d1732a84524b05b8b89e7b
sych74 <sych74@gmail.com>
2021-07-19 11:38:38 +0300

    Update optimization.sh

--- llsmp/default/magento/usr/local/sbin/optimization.sh
+++ llsmp/default/magento/usr/local/sbin/optimization.sh
@@ -8,6 +8,8 @@ ED_CMD="ed --inplace"
 
 if [ -e ${DEFAULT_VIRTUALHOST_CONFIG} ]; then
   /usr/bin/xmlstarlet ${ED_CMD} -u "virtualHostConfig/docRoot" -v "\$VH_ROOT/ROOT/pub/" "${DEFAULT_VIRTUALHOST_CONFIG}" 2>/dev/null;
+  /usr/bin/xmlstarlet ${ED_CMD} -s "virtualHostConfig/cache/storage" -t elem -n "litemage" -v "1" "${DEFAULT_VIRTUALHOST_CONFIG}" 2>/dev/null;
+  /usr/bin/xmlstarlet ${ED_CMD} -u "virtualHostConfig/cache/storage/litemage" -v "1" "${DEFAULT_VIRTUALHOST_CONFIG}" 2>/dev/null;
 fi
 
 sudo jem service restart
