c3fc31ccaa8511ec7cd8526176bef799a6c81483
DmytroZubelevych <31444413+DmytroZubelevych@users.noreply.github.com>
2021-10-12 11:22:16 +0300

    Update optimization.sh
    JE-59934 [WP Cluster] Refactor the optimization.sh to check the LiteSpeed config value before modification

--- litespeedphp/default/wordpress/usr/local/sbin/optimization.sh
+++ litespeedphp/default/wordpress/usr/local/sbin/optimization.sh
@@ -13,12 +13,17 @@ cp -f "${DEFAULT_VIRTUALHOST_CONFIG}" ${DEFAULT_VIRTUALHOST_CONFIG}.backup.$(dat
 
 if [ -e ${DEFAULT_LSWS_CONFIG} ]; then 
 ###JE-51695-Disable compression on cp layer for WP cluster
-  /usr/bin/xmlstarlet $ED_CMD -u "httpServerConfig/tuning/enableGzipCompress" -v "0" ${DEFAULT_LSWS_CONFIG} 2>&1;
+  CURRENT_VALUE=$(xmlstarlet sel -t -v "httpServerConfig/tuning/enableGzipCompress" ${DEFAULT_LSWS_CONFIG})
+  if [ "x${CURRENT_VALUE}" != "x0" ]; then
+    /usr/bin/xmlstarlet $ED_CMD -u "httpServerConfig/tuning/enableGzipCompress" -v "0" ${DEFAULT_LSWS_CONFIG} 2>&1;
+  fi
 fi
 
 if [ -e ${DEFAULT_VIRTUALHOST_CONFIG} ]; then
-  /usr/bin/xmlstarlet ${ED_CMD} -d "virtualHostConfig/cache/storage/cacheStorePath" "${DEFAULT_VIRTUALHOST_CONFIG}" 2>/dev/null;
+  CURRENT_VALUE=$(xmlstarlet sel -t -v "virtualHostConfig/cache/storage/cacheStorePath" ${DEFAULT_VIRTUALHOST_CONFIG})
+  if [ -n "${CURRENT_VALUE}" ]; then
+    /usr/bin/xmlstarlet ${ED_CMD} -d "virtualHostConfig/cache/storage/cacheStorePath" "${DEFAULT_VIRTUALHOST_CONFIG}" 2>/dev/null;
+  fi
 fi
 
 sudo service lsws reload
-
