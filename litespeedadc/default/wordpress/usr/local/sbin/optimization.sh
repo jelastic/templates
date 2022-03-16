b1102f68815354489cf3342ebf78b25b98855196
sych74 <sych74@gmail.com>
2022-03-16 01:05:05 +0100

    Update optimization.sh

--- litespeedadc/default/wordpress/usr/local/sbin/optimization.sh
+++ litespeedadc/default/wordpress/usr/local/sbin/optimization.sh
@@ -10,7 +10,6 @@ cp -f ${LSLB_CONF} ${LSLB_CONF}.backup.$(date +%d-%m-%Y.%H:%M:%S.%N) || exit 1
 /usr/bin/xmlstarlet ${ED_CMD} -u "loadBalancerConfig/loadBalancerList/loadBalancer[name = "*"]/workerGroupList/workerGroup[name = "*"]/maxConns" -v "150" "${LSLB_CONF}" 2>/dev/null;
 /usr/bin/xmlstarlet ${ED_CMD} -u "loadBalancerConfig/loadBalancerList/loadBalancer[name = "*"]/workerGroupList/workerGroup[name = "*"]/retryTimeout" -v "5" "${LSLB_CONF}" 2>/dev/null;
 /usr/bin/xmlstarlet ${ED_CMD} -u "loadBalancerConfig/loadBalancerList/loadBalancer[name = "*"]/workerGroupList/workerGroup/pingInterval" -v "1" "${LSLB_CONF}" 2>/dev/null;
-/usr/bin/xmlstarlet ${ED_CMD} -u "loadBalancerConfig/loadBalancerList/loadBalancer[name = "*"]/strategy" -v "3" "${LSLB_CONF}" 2>/dev/null;
 
 #Enable  cachePolicy
 /usr/bin/xmlstarlet ${ED_CMD} -d "virtualHostConfig/cache/cachePolicy" "${VH_CONF}" 2>/dev/null;
