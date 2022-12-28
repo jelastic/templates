91e66dbfb94ff3f3a153b81fe589bb90bbddf27e
GS <gs@jelastic.com>
2022-12-28 10:24:24 +0000

    Fixed lsphp memory limit

---
+++ llsmp/default/wordpress/usr/local/sbin/optimization.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DEFAULT_LSWS_CONFIG="/var/www/conf/httpd_config.xml"
+ED_CMD="ed --inplace"
+
+cp -f "${DEFAULT_LSWS_CONFIG}" ${DEFAULT_LSWS_CONFIG}.backup.$(date +%d-%m-%Y.%H:%M:%S.%N) || exit 1
+
+/usr/bin/xmlstarlet $ED_CMD -u "httpServerConfig/extProcessorList/extProcessor/memSoftLimit" -v "10240M" ${DEFAULT_LSWS_CONFIG} 2>&1;
+/usr/bin/xmlstarlet $ED_CMD -u "httpServerConfig/extProcessorList/extProcessor/memHardLimit" -v "10240M" ${DEFAULT_LSWS_CONFIG} 2>&1;
+
