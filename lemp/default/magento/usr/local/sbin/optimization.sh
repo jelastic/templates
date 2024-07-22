6b84ae864ce43e9af56d58fc7955c9964651804d
Henadii Sychevskyi <sych74@gmail.com>
2024-07-22 10:29:48 +0200

    Update optimization.sh

--- lemp/default/magento/usr/local/sbin/optimization.sh
+++ lemp/default/magento/usr/local/sbin/optimization.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
 
-SSL_CONFIG_FILE="/etc/nginx/conf.d/ssl.conf.disabled"
-LOG_FILE="/var/log/run.log"
-
-if [ -f "$SSL_CONFIG_FILE" ]; then
-    echo "$(date): Removing QUIC support..." | tee -a "$LOG_FILE"
-    sed -i '/listen\s\+443\s\+quic/d; /listen\s\+\[::\]:443\s\+quic/d' "$SSL_CONFIG_FILE"
-    echo "$(date): QUIC support removed from $SSL_CONFIG_FILE" | tee -a "$LOG_FILE"
-fi
